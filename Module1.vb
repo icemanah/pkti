480918030a53a3fcec9a0b496e681385a266dc59
Arwin Halim <win.halim@gmail.com>
2013-10-18 09:41:43 +0700

    tambah module1.vb dan folder 1

---
+++ Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim harga As Double
+        Console.Write("Harga barang: ")
+        harga = Convert.ToDouble(Console.ReadLine)
+        Dim diskonx As Double = diskon(harga)
+        Console.WriteLine("Harga diskon: " + diskonx)
+        Console.ReadKey()
+    End Sub
+
+    Function diskon(h As Double) As Double
+        Console.Write("")
+        Console.Write("")
+        Console.Write("")
+        Console.Write("")
+        Console.Write("")
+        Return 0.2 * h
+    End Function
+
+
+
+End Module
